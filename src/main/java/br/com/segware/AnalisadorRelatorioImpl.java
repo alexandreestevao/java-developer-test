6dc2b4fbb680220bc01ea71ef4966b2e8b46c3e1
alexandreestevao <alexandresestevao@gmail.com>
2017-03-21 16:44:26 -0300

    Refatoração do Código e Adição dos Comentários

--- src/main/java/br/com/segware/AnalisadorRelatorioImpl.java
+++ src/main/java/br/com/segware/AnalisadorRelatorioImpl.java
@@ -18,9 +18,8 @@ public class AnalisadorRelatorioImpl implements IAnalisadorRelatorio {
 
     public void processaArquivo(File path) throws IOException {
 
-        List<QuantidadeTipoHolder> quantidadeTipoHolderList = new ArrayList<>();
         Map<Tipo,Integer> quantidadeOcorrenciaTipo = new HashMap<>();
-        
+
         Map<String,List<Long>> temposMediosPorAtendente = new HashMap<>();
         Map<String,Evento> ultimoEventoAlarmePorCliente = new HashMap<>();
 
@@ -28,49 +27,115 @@ public class AnalisadorRelatorioImpl implements IAnalisadorRelatorio {
 
         for(String linha : linhas) {
 
-            Integer quantidadeEventos = 1;
-            Integer tipoQuantidade = 1;
-
             Evento evento = carregaArquivo(linha);
 
-            if (eventosCliente.containsKey(evento.getCodigoCliente())) {
-                quantidadeEventos = eventosCliente.get(evento.getCodigoCliente());
-                quantidadeEventos++;
-            }
+            agrupaEventoPorCliente(evento);
+            agrupaQuantidadeOcorrenciaPorEvento(quantidadeOcorrenciaTipo,evento);
+            agrupaAtendenteTemposMedios(temposMediosPorAtendente,evento);
+            processaEventoAlarmeDesarme(ultimoEventoAlarmePorCliente,evento);
+            atualizaUltimoEvento(ultimoEventoAlarmePorCliente,evento);
+        }
 
-            eventosCliente.put(evento.getCodigoCliente(),quantidadeEventos);
+        processaQuantidadeOcorrenciaPorTipo(quantidadeOcorrenciaTipo);
+        descobreTemposMedios(temposMediosPorAtendente);
+    }
 
-            if (quantidadeOcorrenciaTipo.containsKey(evento.getTipoEvento())) {
-                tipoQuantidade = quantidadeOcorrenciaTipo.get(evento.getTipoEvento());
-                tipoQuantidade++;
-            }
+    /**
+     * Atualiza o mapa de ultimo evento por cliente, caso seja um evento de alarme.
+     * @param ultimoEventoAlarmePorCliente - Mapa para guardar o ultimo evento de alarme de cada cliente
+     * @param evento - Evento sendo atualmente processado
+     */
+    private void atualizaUltimoEvento(Map<String, Evento> ultimoEventoAlarmePorCliente, Evento evento) {
 
-            quantidadeOcorrenciaTipo.put(evento.getTipoEvento(),tipoQuantidade);
+        if(evento.getTipoEvento().equals(Tipo.ALARME)) {
+            ultimoEventoAlarmePorCliente.put(evento.getCodigoCliente(),evento);
+        }
+    }
 
-            List<Long> temposMedios = new ArrayList<>();
-            if (temposMediosPorAtendente.containsKey(evento.getCodigoAtendente())) {
-                temposMedios = temposMediosPorAtendente.get(evento.getCodigoAtendente());
-            }
 
-            temposMedios.add(evento.getDiferencaFimInicio());
-            temposMediosPorAtendente.put(evento.getCodigoAtendente(),temposMedios);
+    /**
+     * Agrupa os eventos por cada cliente para preecher o mapa de eventosCliente
+     * @param evento - Evento sendo atualmente processado
+     */
 
-            if (ultimoEventoAlarmePorCliente != null && evento.getTipoEvento().equals(Tipo.DESARME)) {
-                Evento ultimoEvento = ultimoEventoAlarmePorCliente.get(evento.getCodigoCliente());
+    private void agrupaEventoPorCliente(Evento evento){
 
-                if(ultimoEvento!=null){
-                    int diferencaMinutos = Minutes.minutesBetween(ultimoEvento.getDataInicio(),evento.getDataInicio()).getMinutes();
-                    if(diferencaMinutos>=0 && diferencaMinutos<=5){
-                        codigoSequencialEventosDesarme.add(evento.getCodigoSequencial());
-                    }
+        Integer quantidadeEventos = 1;
+
+        if (eventosCliente.containsKey(evento.getCodigoCliente())) {
+            quantidadeEventos = eventosCliente.get(evento.getCodigoCliente());
+            quantidadeEventos++;
+        }
+
+        eventosCliente.put(evento.getCodigoCliente(),quantidadeEventos);
+
+    }
+
+    /**
+     * Agrupa a quantidade de ocorrencia por cada tipo de evento para depois ordenar
+     * @param quantidadeOcorrenciaTipo - Mapa com o agrupamento de Tipo de evento com a quantidade de ocorrencias
+     * @param evento - Evento sendo atualmente processado
+     */
+    private void agrupaQuantidadeOcorrenciaPorEvento(Map<Tipo,Integer> quantidadeOcorrenciaTipo, Evento evento) {
+
+        Integer tipoQuantidade = 1;
+        if (quantidadeOcorrenciaTipo.containsKey(evento.getTipoEvento())) {
+            tipoQuantidade = quantidadeOcorrenciaTipo.get(evento.getTipoEvento());
+            tipoQuantidade++;
+        }
+
+        quantidadeOcorrenciaTipo.put(evento.getTipoEvento(), tipoQuantidade);
+    }
+
+    /**
+     * Agrupa para cada atendente a lista de tempos de duracao de cada evento
+     * @param temposMediosPorAtendente - Mapa com o agrupamento de atendente com os seus tempos
+     * @param evento - Evento sendo atualmente processado
+     */
+    private void agrupaAtendenteTemposMedios(Map<String,List<Long>> temposMediosPorAtendente, Evento evento) {
+
+        List<Long> temposMedios = new ArrayList<>();
+        if (temposMediosPorAtendente.containsKey(evento.getCodigoAtendente())) {
+            temposMedios = temposMediosPorAtendente.get(evento.getCodigoAtendente());
+        }
+
+        temposMedios.add(evento.getDiferencaFimInicio());
+        temposMediosPorAtendente.put(evento.getCodigoAtendente(), temposMedios);
+
+    }
+
+    /**
+     * Processa o evento de alarme seguido por desarme
+     * Verifica se o evento sendo processado e de desarme. Caso seja verifica-se o ultimo evento de alarme do mesmo cliente. Caso o tempo seja ate 5 minutos guarda o codigo sequencial
+     * @param ultimoEventoAlarmePorCliente - O ultimo evento de Alarma de um determinado cliente
+     * @param evento - Evento sendo atualmente processado
+     */
+    private void processaEventoAlarmeDesarme(Map<String,Evento> ultimoEventoAlarmePorCliente, Evento evento) {
+
+        if (ultimoEventoAlarmePorCliente != null && evento.getTipoEvento().equals(Tipo.DESARME)) {
+            Evento ultimoEvento = ultimoEventoAlarmePorCliente.get(evento.getCodigoCliente());
+
+            if(ultimoEvento!=null){
+
+                int diferencaMinutos = Minutes.minutesBetween(ultimoEvento.getDataInicio(),evento.getDataInicio()).getMinutes();
+                if(diferencaMinutos>=0 && diferencaMinutos<=5){
+                    codigoSequencialEventosDesarme.add(evento.getCodigoSequencial());
                 }
             }
-
-            if(evento.getTipoEvento().equals(Tipo.ALARME)) {
-                ultimoEventoAlarmePorCliente.put(evento.getCodigoCliente(),evento);
-            }
-
         }
+    }
+
+    /**
+     *  Processo o mapa de quantidadeOcorrenciaTipo para identificar a ordem decrescente dos eventos.
+     *  Itera o mapa de quantidade de ocorrencia por tipo para uma lista de uma classe que possui uma comparacao por quantidade.
+     *  Atraves da implementacao de Comparable<QuantidadeTipoHolder> permite ordenar por ordem crescente e posteriormente reverte a lista para ordem decrescente.
+     *  Por fim itera a lista em ordem decrescente pegando os tipos que QuantidadeTipoHolder possui
+     *
+     * @param quantidadeOcorrenciaTipo - O mapa de quantidade de ocorrencia por tipo
+     */
+    private void processaQuantidadeOcorrenciaPorTipo(Map<Tipo,Integer> quantidadeOcorrenciaTipo){
+
+        List<QuantidadeTipoHolder> quantidadeTipoHolderList = new ArrayList<>();
 
         for(Map.Entry<Tipo, Integer> entry : quantidadeOcorrenciaTipo.entrySet()) {
             Tipo tipo = entry.getKey();
@@ -85,6 +150,13 @@ public class AnalisadorRelatorioImpl implements IAnalisadorRelatorio {
         for(QuantidadeTipoHolder valor:quantidadeTipoHolderList) {
             tiposOrdenados.add(valor.getTipo());
         }
+    }
+
+    /**
+     * Atraves do mapa de tempos por atendente, descobre-se o tempo medio de cada atendente e dividi-se pelo total de eventos.
+     * @param temposMediosPorAtendente - Mapa de tempos por atendente
+     */
+    private void descobreTemposMedios(Map<String,List<Long>> temposMediosPorAtendente){
 
         for(Map.Entry<String, List<Long>> entry : temposMediosPorAtendente.entrySet()) {
             String atendente = entry.getKey();
@@ -96,6 +168,7 @@ public class AnalisadorRelatorioImpl implements IAnalisadorRelatorio {
             tempoMedio.put(atendente,total / temposMedios.size());
         }
     }
+
 
     @Override
     public Map<String, Integer> getTotalEventosCliente() {
